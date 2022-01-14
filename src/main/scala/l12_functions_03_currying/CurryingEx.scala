9955a112a6dddce50dbbf9644c827d0c3aabfe77
ruby4eyes <ruby4eyes@gmail.com>
2022-01-15 00:10:48 +0530

    updated

--- src/main/scala/l12_functions_03_currying/CurryingEx.scala
+++ src/main/scala/l12_functions_03_currying/CurryingEx.scala
@@ -1,18 +1,28 @@
 package l12_functions_03_currying
 
 object CurryingEx {
-  def add(x: Int, y: Int ,z:Int): Int  = x + y +z;
-  //val sum
+  /*
+   * use val(function) or def(method)
+   */
+  def m11(mm: Int,dd: Int ,yyyy:Int , name:String): String  = f"$name=[$mm/$dd/$yyyy]"
+  def m1curried1(mm:Int)(dd:Int)(yyyy:Int)(name:String):String = f"$name=[$mm/$dd/$yyyy]";
 
-  def addcurried1(x:Int)(y:Int)(z:Int):Int =x+y;
+  def m12(mm: Int,dd: Int ,yyyy:Int , name:String)  = f"$name=[$mm/$dd/$yyyy]"
+  def m1curried2(mm:Int)(dd:Int)(yyyy:Int)(name:String) = f"$name=[$mm/$dd/$yyyy]";
 
-  def addcurried2(x:Int) (y:Int)  = (z:Int)=> x+y+z
 
+  def m21:(Int, Int , Int ,String )=> String = (mm,dd,yyyy,name) => f"$name=[$mm/$dd/$yyyy]"
+  def m2curried1: Int => Int => Int => String => String = mm=>dd=>yyyy=>name => f"$name=[$mm/$dd/$yyyy]"
+
+  def m22 = (mm:Int, dd:Int , yyyy:Int ,name:String ) => f"$name=[$mm/$dd/$yyyy]"
+  //def m2curried2 = What will be the curried equivalent ?????
 
   def main(args: Array[String]): Unit = {
-    println(add(20, 19, 21))
-    println(addcurried1(20)(19)(21))
-    println(addcurried2(20)(19)(21))
+    println(m11(20, 12, 1990, "ruby"))
+    println(m1curried1(20) (12) (1990) ("zen"))
+    println(m1curried2(20) (12) (1990) ("ken"))
+    println(m21(20, 12, 1990, "jack"))
+    println(m2curried1(20)(12)(1990)("rose"))
   }
 
 }
