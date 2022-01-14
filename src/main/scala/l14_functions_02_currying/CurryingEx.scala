a282643761ace18f88df4408224adb7cac248450
ruby4eyes <ruby4eyes@gmail.com>
2022-01-14 19:11:44 +0530

    updated

--- src/main/scala/l14_functions_02_currying/CurryingEx.scala
+++ src/main/scala/l14_functions_02_currying/CurryingEx.scala
@@ -1,17 +1,18 @@
 package l14_functions_02_currying
 
 object CurryingEx {
-  def add(x: Int, y: Int): Int  = x + y;
+  def add(x: Int, y: Int ,z:Int): Int  = x + y +z;
+  val sum
 
-  def addcurried1(x:Int)(y:Int):Int =x+y;
+  def addcurried1(x:Int)(y:Int)(z:Int):Int =x+y;
 
-  def addcurried2(x:Int) = (y:Int) => x+y
+  def addcurried2(x:Int) (y:Int)  = (z:Int)=> x+y+z
 
 
   def main(args: Array[String]): Unit = {
-    println(add(20, 19))
-    println(addcurried1(20)(19))
-    println(addcurried2(20)(19))
+    println(add(20, 19, 21))
+    println(addcurried1(20)(19)(21))
+    println(addcurried2(20)(19)(21))
   }
 
 }
